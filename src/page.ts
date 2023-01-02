982815c66fc8b113d4656a316662db6667c41523
lonr <lonr@users.noreply.github.com>
2023-01-02 20:29:58 +0800

    feat(page.ts): colorize output; improve performance

--- src/page.ts
+++ src/page.ts
@@ -1,48 +1,56 @@
 import { readFile } from 'node:fs/promises';
-import { fromMarkdown } from 'mdast-util-from-markdown';
-import type { Root, Content } from 'mdast';
+import chalk from 'chalk';
 
-// refer to https://github.com/danielpza/markdown-to-bbcode/blob/master/index.js
-function render(node: Root | Content): string {
-  switch (node.type) {
-    case 'root':
-      return node.children.map(render).join('\n') + '\n';
-    case 'heading':
-      // ignore the heading (the name of the command)
-      return '';
-    case 'text':
-      return node.value;
-    case 'inlineCode':
-    case 'code':
-      return '  ' + node.value;
-    case 'link':
-      // add `<` and `>` back
-      return '<' + node.children.map(render).join('') + '>';
-    // inline
-    case 'paragraph': {
-      return node.children.map(render).join('');
-    }
-    // add more line breaks
-    case 'list': {
-      // kind of hacky
-      return '\n' + node.children.map(render).join('') + '\n';
-    }
-    // case 'blockquote':
-    // case 'listItem':
-    default: {
-      if ('children' in node) {
-        return node.children.map(render).join('\n');
+const exampleText = chalk.green;
+const exampleCode = chalk.cyan;
+const exampleCodeHighted = chalk.underline;
+
+const spaces = '  ';
+
+function render(mkd: string): string {
+  let res = '';
+  let buffer = '';
+  let i = 0;
+  while (i < mkd.length) {
+    const c = mkd[i];
+    if (c === '\n') {
+      if (buffer === '') {
+        res += '\n';
       } else {
-        return '';
+        switch (buffer[0]) {
+          case '#':
+            break;
+          case '>':
+            res += spaces + buffer.slice(2) + '\n';
+            break;
+          case '-':
+            res += spaces + exampleText(buffer.slice(2)) + '\n';
+            break;
+          case '`':
+            res +=
+              spaces.repeat(2) +
+              exampleCode(
+                buffer
+                  .slice(1, buffer.length - 1)
+                  .replaceAll(/{{([^}]*)}}/g, exampleCodeHighted('$1'))
+              ) +
+              '\n';
+            break;
+        }
+        buffer = '';
       }
+    } else {
+      buffer += c;
     }
+    i += 1;
   }
+  return res;
 }
 
 export class Page {
   constructor(private path: string) {}
 
   async render() {
-    console.log(render(fromMarkdown(await readFile(this.path))));
+    console.log(render(await readFile(this.path, 'utf8')));
   }
 }
