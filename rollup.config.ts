12a79826f2e4e89bbb0e24b3f95130f5517d7936
lonr <lonr@users.noreply.github.com>
2023-01-16 20:48:08 +0800

    build(rollup.config.ts): use rollup to bundle external deps

---
+++ rollup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'rollup';
+import json from '@rollup/plugin-json';
+import rollupTypescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+const config = defineConfig({
+  input: 'src/cli.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+  },
+  plugins: [
+    rollupTypescript(),
+    json(),
+    resolve({ preferBuiltins: true }),
+    commonjs(),
+    terser(),
+  ],
+});
+
+export default config;
